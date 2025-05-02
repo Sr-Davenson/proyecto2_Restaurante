42001dda4bba7888defc20a2602deba718a6cf2d
unknown <dsgil@uniboyaca.edu.co>
2025-05-02 00:03:36 -0500

    David: prueba menu inicio

--- views/inicio.php
+++ views/inicio.php
@@ -4,21 +4,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/styleInicio.css">
     <title>Inicio</title>
 </head>
 
 <body>
+    <div class="menu">
     <a href="AdminCategoria.php">Administrar categoria</a>
-    <br>
     <a href="AdminPlatos.php">Administrar platos</a>
-    <br>
     <a href="AdminMesas.php">Administrar mesas</a>
-    <br>
     <a href="CrearOden.php">Iniciar nueva orden</a>
-    <br>
     <a href="AdminOrdenes.php">Administrar ordenes</a>
-    <br>
     <a href="../index.php">Salir</a>
+    </div>
+    <img class="img" src="../images/login.png" alt="l">
 </body>
 
 </html>
