26aa63968922f5b6748975d50aa740be0ed52a68
Alejandra-66 <ludmolina@uniboyaca.edu.co>
2025-05-15 14:40:03 -0500

    cambios

--- views/Forms/viewsDetalleOrden.php
+++ views/Forms/viewsDetalleOrden.php
@@ -43,7 +43,7 @@ $totalRecaudo = 0;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../CSS/styleCrearOrden.css">
+    <link rel="stylesheet" href="../../CSS/style.Ordenes.css">
     <title>Ver Detalle Orden</title>
 </head>
 
